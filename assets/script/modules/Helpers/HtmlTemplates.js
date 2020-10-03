8bded4239d3f5627930aff5cde7896d3bc039a7c
Marcelo Diament <marcelodiament@gmail.com>
2020-10-03 19:19:25 -0300

    feature: project card layout review [009]

--- assets/script/modules/Helpers/HtmlTemplates.js
+++ assets/script/modules/Helpers/HtmlTemplates.js
@@ -3,31 +3,33 @@
 // }
 
 const buildProjetoCard = projeto => `
-  <article style="border-left: 8px solid ${projeto.corProjeto};" class="bg-light rounded-lg m-2 p-3">
-    <header class="my-3 d-flex flex-column flex-nowrap">
-      <div class="projeto-id"><span class="ml-auto px-2 py-1 badge badge-pill badge-dark">PROJETO #0${projeto.id}</span></div>
-      <div class="d-flex flex-row flex-nowrap justify-content-between align-items-start">
-        <img src="./${projeto.logo}" height="96" width="96" class="my-2 circle border-info">
-        <div class="my-2 d-flex flex-column flex-nowrap col">
-          <h4>${projeto.nome}</h4>
-          <small>Tema: ${projeto.tema}</small>
-          <small>Status: ${projeto.status}</small>
-          <small>Sprint: ${projeto.sprint}</small>
+  <div id="projectCardContainer-${projeto.id}" class="col-12 col-lg-6 col-xl-4 p-0 p-sm-2 p-lg-3">
+    <article style="border-left: 8px solid ${projeto.corProjeto};" class="bg-light rounded-lg mx-0 my-2 p-3 d-flex flex-column flex-nowrap justify-content-between h-100" id="projectCard-${projeto.id}">
+      <header class="mt-4 d-flex flex-column flex-nowrap">
+        <div class="projeto-id"><span class="ml-auto px-2 py-1 badge badge-pill badge-dark">PROJETO #0${projeto.id}</span></div>
+        <div class="d-flex flex-row flex-nowrap justify-content-between align-items-start">
+          <img src="./${projeto.logo}" height="80" width="80" class="my-2 rounded-circle border-info">
+          <div class="my-2 d-flex flex-column flex-nowrap col">
+            <h4>${projeto.nome}</h4>
+            <small>Tema: ${projeto.tema}</small>
+            <small>Status: ${projeto.status}</small>
+            <!--<small>Sprint: ${projeto.sprint}</small>-->
+          </div>
         </div>
+      </header>
+      <div class="d-flex flex-column flex-nowrap">
+        <!-- <small>Criado em: ${new Date(projeto.createdAt).toLocaleDateString()} | Atualizado em: ${new Date(projeto.updatedAt).toLocaleDateString()}</small>-->
+        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modalProjeto${projeto.id}">Info</button>
       </div>
-    </header>
-    <div class="d-flex flex-column flex-nowrap">
-      <!-- <small>Criado em: ${new Date(projeto.createdAt).toLocaleDateString()} | Atualizado em: ${new Date(projeto.updatedAt).toLocaleDateString()}</small>-->
-      <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modalProjeto${projeto.id}">Info</button>
-    </div>
-  </article>
+    </article>
+  </div>
 `
 
 const buildProjetos = (projetos, title, description) => {
   let html = `
-    <div>
-      <h2 class="mx-2 mt-3">${title}</h2>
-      <p class="mx-2 mb-3">${description}</p>
+    <div class="row mx-0">
+      <h2 class="col-12 px-1 mx-0 mt-3">${title}</h2>
+      <p class="col-12 px-1 mx-0 mb-3">${description}</p>
   `
   for (let projeto of projetos) {
     html += buildProjetoCard(projeto)
